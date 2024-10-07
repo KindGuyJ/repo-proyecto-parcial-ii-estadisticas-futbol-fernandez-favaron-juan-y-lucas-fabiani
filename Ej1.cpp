8a44f29a85bbe6b708e656ffa76f3f514f596e78
Juan <jiffavaron@gmail.com>
2024-10-07 10:49:58 -0300

    Limpieza

--- Ej1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-    cout << "Ejercicio N° 1" << endl;
-
-    return 0;
-}
--- Ej2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-    cout << "Ejercicio N° 2" << endl;
-
-    return 0;
-}
