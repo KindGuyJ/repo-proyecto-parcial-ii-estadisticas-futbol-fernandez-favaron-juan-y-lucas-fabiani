eb2c49657dd40a02a88fb24ab43e8d4ce262035f
Lucas Fabiani <145565150+LucasUCC-hub@users.noreply.github.com>
2024-10-07 14:06:12 +0000

    ui simple

---
+++ ui.cpp
@@ -0,0 +1,7 @@
+#include "imgui/imgui.h"
+
+ImGui::Text("Hello, world %d", 123);
+if (ImGui::Button("Save"))
+    MySaveFunction();
+ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
