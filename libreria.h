1108a1cb655a4f248e114c26bd6f6ff83c009513
Juan <jiffavaron@gmail.com>
2024-10-07 18:57:31 -0300

    Carga basica de datos a cupholder

--- libreria.h
+++ libreria.h
@@ -1,3 +1,101 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
+
+struct partido
+{
+    string Jornada;
+    string Fecha;
+    string EquipoLocal;
+    string GolesLocal;
+    string GolesVisitante;
+    string EquipoVisitante;
+    string Competicion;
+};
+
+// Capaz cambiar el asignar y get para que den el objeto pero bueno no los use para nada todavia.
+class dataclass
+{
+private:
+    vector<partido> data;
+
+public:
+    void add(partido s)
+    {
+        data.push_back(s);
+    }
+    int size()
+    {
+        return data.size();
+    }
+    /// @brief 
+    /// @param o A cual de todos los vectores asignar
+    /// @param x Nuevo string
+    /// @param i [0-6] 0-Jornada; 1-Fecha; 2-EquipoLocal; 3-GolesLocal; 4-GolesVisitante; 5-EquipoVisitante; 6-Competicion;
+    void assign(int o, string x, int i)
+    {
+        switch (i)
+        {
+        case 0:
+            data[o].Jornada = x;
+            break;
+        case 1:
+            data[o].Fecha = x;
+            break;
+        case 2:
+            data[o].EquipoLocal = x;
+            break;
+        case 3:
+            data[o].GolesLocal = x;
+            break;
+        case 4:
+            data[o].GolesVisitante = x;
+            break;
+        case 5:
+            data[o].EquipoVisitante = x;
+            break;
+        case 6:
+            data[o].Competicion = x;
+            break;
+        default:
+            throw 666 - 1;
+            break;
+        }
+    }
+    /// @brief 
+    /// @param o A cual vector leer
+    /// @param i [0-6] 0-Jornada; 1-Fecha; 2-EquipoLocal; 3-GolesLocal; 4-GolesVisitante; 5-EquipoVisitante; 6-Competicion;
+    /// @return la data
+    string get(int o, int i)
+    {
+        switch (i)
+        {
+        case 0:
+            return data[o].Jornada;
+            break;
+        case 1:
+            return data[o].Fecha;
+            break;
+        case 2:
+            return data[o].EquipoLocal;
+            break;
+        case 3:
+            return data[o].GolesLocal;
+            break;
+        case 4:
+            return data[o].GolesVisitante;
+            break;
+        case 5:
+            return data[o].EquipoVisitante;
+            break;
+        case 6:
+            return data[o].Competicion;
+            break;
+        default:
+            throw 666 - 2;
+            break;
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
 #include"libreria.h"
+#include <sstream> //para stream
 
+int main(){
+    dataclass cupholder;
+    string linea;
+    ifstream archivo("Base_Datos_COMA.csv");
+    getline(archivo,linea);
+    
+    while (getline(archivo, linea)) {
+        stringstream stream(linea);
+        // Jornada,Fecha,Equipo Local,Goles Local,Goles Visitante,Equipo Visitante,Competicion
+        partido carga;
+        getline(stream,carga.Jornada,',');
+        getline(stream,carga.Fecha,',');
+        getline(stream,carga.EquipoLocal,',');
+        getline(stream,carga.GolesLocal,',');
+        getline(stream,carga.GolesVisitante,',');
+        getline(stream,carga.EquipoVisitante,',');
+        getline(stream,carga.Competicion,',');
+        //Aca pasa la magia pero no vale mirar atras de la cortina :(
+        cupholder.add(carga);
+    }
+
+    archivo.close();
+}
